f1f392a85b8980b236d6e7b6e3dbd4f9b0f1305f
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-02-02 16:48:24 +0200

    Basic Express App

--- api/app.js
+++ api/app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler.js');
+
+app.use(express.json());
+// routes
+app.get('/', (req, res) =>
+  res.send('<h1>Store API</h1><a href="/api/v1/products">product route</a>')
+);
+//product routes
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+//vars
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connect to DB
+    app.listen(port, console.log(`Server listen on port ${port}...`));
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+start();
