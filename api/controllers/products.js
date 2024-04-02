4466dcd19ed987cbd797120929c0cceb6cc6a021
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-04-02 23:37:30 +0300

    add numeric filter

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,13 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductStatic = async (_req, res) => {
-  const search = 'enter';
-  const products = await Product.find({}).sort('name price');
+  const products = await Product.find({ price: { $lt: 20 } });
   res.status(200).json({ products, length: products.length });
 };
 
 const getAllProduct = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -17,6 +16,27 @@ const getAllProduct = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+    filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
   let result = Product.find(queryObject);
   if (sort) {
@@ -29,11 +49,13 @@ const getAllProduct = async (req, res) => {
     const fieldList = fields.split(',').join(' ');
     result = result.select(fieldList);
   }
+
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
   result.limit(limit).skip(skip);
 
+  console.log('getAllProduct ~ queryObject:', queryObject);
   const products = await result;
   res.status(200).json({ products, page: page, length: products.length });
 };
