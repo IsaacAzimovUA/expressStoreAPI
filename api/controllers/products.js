a90f933beddc161bc323b9249d34c70b98b5b3f6
IsaacAzimovUA <109369640+IsaacAzimovUA@users.noreply.github.com>
2024-04-02 21:09:33 +0300

    add pagination

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -29,8 +29,13 @@ const getAllProduct = async (req, res) => {
     const fieldList = fields.split(',').join(' ');
     result = result.select(fieldList);
   }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result.limit(limit).skip(skip);
+
   const products = await result;
-  res.status(200).json({ products, length: products.length });
+  res.status(200).json({ products, page: page, length: products.length });
 };
 
 module.exports = { getAllProductStatic, getAllProduct };
